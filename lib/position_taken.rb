c66387ee9ed448196841c8f5c18af219e000ff2c
hela karaa <hela.karaa@ensi-uma.tn>
2018-07-23 09:17:25 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken(index,board)
   if board[index]==" " || board[index] ==""
      return false
-  else if board[index]=="X" || board[index]=="O"
+  elsif board[index]=="X" || board[index]=="O"
      return true
   end
 end
