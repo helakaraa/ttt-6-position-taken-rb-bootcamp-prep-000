fa6e65c9c78f6946b838b51dfbd8846372c9b7b8
hela karaa <hela.karaa@ensi-uma.tn>
2018-07-23 09:17:16 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken(index,board)
-  if board[index]==" " || board[index]==""
+  if board[index]==" " || board[index] ==""
      return false
   else if board[index]=="X" || board[index]=="O"
      return true
